bba918d7a0336d5dc8d259711dcee046a60d5f02
raquel.paternazi <raquel.paternazi@BR-IT00182.globant.com>
2021-02-10 19:58:57 -0300

    Alterações no comentário

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -15,16 +15,15 @@ import java.util.Scanner;
  * @author raquel.paternazi
  * data: 03/02/2021
  */
-public class Principal { //Classe que contém o método principal.
+public class Principal { 
 
-	public static void main(String[] args) { //Este é o método principal.
+	public static void main(String[] args) { 
 		System.out.println("Olá amigo!\nQual é o seu nome?");
 		
-		//Ler uma string no console
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		
-		//Exibir um String usando o printf
+
 		System.out.printf("Olá %s", nome);
 		in.close();
 	}
